c7a7f010d9e6ada978194416feb1b5544218d67c
Kevin Salina <kevin.salina25@gmail.com>
2021-09-07 11:29:53 -0400

    populating linking table works

--- utilities/parseCSV.js
+++ utilities/parseCSV.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const csv = require('fast-csv')
 const models = require('../models')
+const { model } = require('../config/database')
 
 const path = process.cwd() + '/novels.csv'
 const novels = []
@@ -11,7 +12,7 @@ fs.createReadStream(path)
   .pipe(csv.parse({ headers: ['title', 'author', 'genre'], renameHeaders: true }))
   .on('error', error => console.error(error))
   .on('data', row => novels.push(row))
-  .on('end', () => {
+  .on('end', async () => {
     let novelGenres = novels.map(({ author, ...rest }) => {
       rest.genre = rest.genre.split(',')
 
@@ -20,9 +21,33 @@ fs.createReadStream(path)
 
     let i = 1
 
+    const titles = await models.Title.findAll()
+    const genres = await models.Genre.findAll()
+
+    const titleByName = titles.reduce((a, title) => {
+      a[title.name] = title
+
+      return a
+    }, {})
+
+    const genreByName = genres.reduce((a, genre) => {
+      a[genre.name] = genre
+
+      return a
+    }, {})
+
+
+
     for (const novel of novelGenres) {
       for (const genre of novel.genre) {
-        console.log(`INSERT INTO genres (titleId, genreId) VALUES (${i}, '${genre}')`)
+        // console.log(`INSERT INTO genres (titleId, genreId) VALUES (${i}, '${genre}')`)
+        const titleId = titleByName[novel.title].id
+        const genreId = genreByName[genre].id
+
+        await models.TitleGenre.create({
+          titleId,
+          genreId
+        })
       }
 
       i++
